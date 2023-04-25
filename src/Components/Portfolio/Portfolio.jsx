16bff2b571d3c0b1feebc86156c2e46761be6ab9
constantin_m <cmitrokhin@applaudostudios.dev>
2023-04-25 17:38:21 -0300

    Video

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -11,9 +11,10 @@ export default function Portfolio({id}) {
     <div className={styles.portfolioContainer} id={id}>
         <h1>{t("portfolio_title")}</h1>
         <div className={styles.videosContainer}>
-          <video src="url_del_video1.mp4" controls className={styles.video} />
-          <video src="url_del_video2.mp4" controls className={styles.video} />
-          <video src="url_del_video3.mp4" controls className={styles.video} />
+          {/* <video src="https://youtu.be/vyjot-8z66E" controls className={styles.video} /> */}
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/vyjot-8z66E" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen style={{width: "100%"}}></iframe>
+          {/* <video src="url_del_video2.mp4" controls className={styles.video} />
+          <video src="url_del_video3.mp4" controls className={styles.video} /> */}
         </div>
     </div>
 
